fb68fdaeaf2a9ae2662a0f57975e9c4b95c8e3b2
Eh Akash <ehakash01@gmail.com>
2021-02-02 17:22:54 +0600

    True vs False

---
+++ trueFalse.js
@@ -0,0 +1,20 @@
+// Falsy:
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+
+// Truthy:
+// "0" , " " , [] , {}
+
+
+let name = 0;
+if (name || name == 0) {
+    console.log("Condition is true");
+}
+else {
+    console.log("Condition is False");
+}
